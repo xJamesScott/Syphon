feeff95558e55a14333f9f6fea70de36f7089a63
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-05-20 21:11:32 -0400

    auth edits

---
+++ components/Auth0.js
@@ -0,0 +1,166 @@
+const axios = require("axios");
+const httpErrors = require("http-errors");
+const qs = require("querystring");
+const { config } = require("../../config"); // env-schema
+
+const {
+  auth: { domain: AUTH_DOMAIN },
+} = config;
+
+const getAuthHeaders = (token) => {
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
+// todo: check improving the error handling here, could also check & handle or pass the response message e.g.
+/*
+    data: {
+      error: 'invalid_grant',
+      error_description: 'Wrong email or password.'
+    }
+ */
+const wrapError = (err) => {
+  const {
+    response: { data, status },
+  } = err;
+  const { message } = data;
+  const statusCode = status || data.statusCode;
+  throw httpErrors(statusCode, message);
+};
+
+const buildOauthUrl = (connection, redirectTo) => {
+  const qParams = qs.stringify({
+    // TODO - AUTH0 UPDATE PARAMS
+    
+    audience: `${AUTH_DOMAIN}/api/v2/`,
+    client_id: config.auth.clientId,
+    connection,
+    redirect_uri: redirectTo,
+    response_type: "code",
+    scope: "openid email profile",
+    state: config.auth.state,
+  });
+  return `${AUTH_DOMAIN}/authorize?${qParams}`;
+};
+
+const authenticate = async (grantType, payload = {}) => {
+  const body = {
+    // TODO - AUTH0 UPDATE PARAMS
+    audience: `${AUTH_DOMAIN}/api/v2/`,
+    client_id: config.auth.clientId,
+    client_secret: config.auth.secretKey,
+    grant_type: grantType,
+    ...payload,
+  };
+  try {
+    const res = await axios.post(`${AUTH_DOMAIN}/oauth/token`, body);
+    return res.data.access_token;
+  } catch (err) {
+    return wrapError(err);
+  }
+};
+
+const createUser = async (token, payload) => {
+  try {
+    const res = await axios.post(
+      `${AUTH_DOMAIN}/api/v2/users`,
+      payload,
+      getAuthHeaders(token),
+    );
+    return res.data;
+  } catch (err) {
+    return wrapError(err);
+  }
+};
+
+const getUser = async (token) => {
+  try {
+    const res = await axios.get(
+      `${AUTH_DOMAIN}/userinfo`,
+      getAuthHeaders(token),
+    );
+    return res.data;
+  } catch (err) {
+    return wrapError(err);
+  }
+};
+
+const updateUser = async (token, userId, payload) => {
+  try {
+    const res = await axios.patch(
+      `${AUTH_DOMAIN}/api/v2/users/${userId}`,
+      payload,
+      getAuthHeaders(token),
+    );
+    return res.data;
+  } catch (err) {
+    return wrapError(err);
+  }
+};
+
+const sendChangePasswordEmail = async (token, email) => {
+  const client_id = config.auth.clientId;
+  const connection = "Username-Password-Authentication";
+  const payload = {
+    client_id,
+    email,
+    connection,
+  };
+  try {
+    const res = await axios.post(
+      `${AUTH_DOMAIN}/dbconnections/change_password`,
+      payload,
+      getAuthHeaders(token),
+    );
+    return res.data;
+  } catch (err) {
+    return wrapError(err);
+  }
+};
+
+const linkAccounts = async (token, rootUserId, targetUserId) => {
+  const parts = targetUserId.split("|"); // "provider|user_id"
+  const payload = {
+    provider: parts[0],
+    user_id: parts[1],
+  };
+  try {
+    const res = await axios.post(
+      `${AUTH_DOMAIN}/api/v2/users/${rootUserId}/identities`,
+      payload,
+      getAuthHeaders(token),
+    );
+    return res.data;
+  } catch (err) {
+    return wrapError(err);
+  }
+};
+
+const getAccountsWithSameEmail = async (token, email, forVerified) => {
+  try {
+    const res = await axios.get(`${AUTH_DOMAIN}/api/v2/users`, {
+      params: {
+        q: `email:"${email}"${forVerified ? " AND email_verified:true" : ""}`,
+        search_engine: "v3",
+      },
+      ...getAuthHeaders(token),
+    });
+    return res.data;
+  } catch (err) {
+    return wrapError(err);
+  }
+};
+
+module.exports = {
+  authenticate,
+  buildOauthUrl,
+  createUser,
+  getAccountsWithSameEmail,
+  getUser,
+  linkAccounts,
+  sendChangePasswordEmail,
+  updateUser,
+};
---
+++ utils/utils.js
@@ -0,0 +1,32 @@
+const _isEmail = require("validator/lib/isEmail");
+
+
+const getCookieToken = (req) => req.cookies.token;
+
+const isValidEmail = (email) => {
+  return _isEmail(email) && isValidTopLevelDomain(email);
+};
+
+// password validation code from front-end 'src\utils\validators'
+const SPECIAL_CHARS = /[!@#$%^&*.;]/;
+const containsLowerCase = (str) => /[a-z]/.test(str);
+const containsUpperCase = (str) => /[A-Z]/.test(str);
+const containsNumber = (str) => /\d/.test(str);
+const containsSpecialChars = (str) => SPECIAL_CHARS.test(str);
+
+const isValidPassword = (password) => {
+  const passwordChecks = [
+    containsLowerCase(password),
+    containsUpperCase(password),
+    containsNumber(password),
+    containsSpecialChars(password),
+  ];
+
+  return passwordChecks.filter((isValid) => isValid).length >= 3;
+};
+
+
+module.exports = {
+  isValidEmail,
+  isValidPassword,
+};
