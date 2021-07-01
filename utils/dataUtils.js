f6a09db5386cde401d64a69e2396daa2e79dbc11
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-07-01 15:52:09 -0400

    group cart items logic

--- utils/dataUtils.js
+++ utils/dataUtils.js
@@ -2,9 +2,12 @@ const testData = [
     { productId: "a1", name: "prod1", type: "aaa", price: 20 },
     { productId: "a1", name: "prod1", type: "aaa", price: 20 },
     { productId: "a1", name: "prod1", type: "aaa", price: 20 },
+    { productId: "a2", name: "prod2", type: "aaa", price: 20 },
+    { productId: "a2", name: "prod2", type: "aaa", price: 20 },
 ]
 
 let groupedBy = (array, key) => { // groups array of objects by key
+    // console.log({ "groupedBy array": array })
     return array.reduce((result, obj) => {
         (result[obj[key]] = result[obj[key]] || []).push(obj);
         return result;
@@ -12,12 +15,13 @@ let groupedBy = (array, key) => { // groups array of objects by key
 };
 
 
-
-const combine = (valObj) => { // combines group and sums values
-    const prod = valObj[0].productId
+const combine = (valObj, i) => { // combines group and sums values
+    const prod = valObj[i].productId;
     const result = { [prod]: {} };
+
     valObj.forEach(products => {
-        for (let [key, value] of Object.entries(products)) {
+        const prodObj = Object.entries(products)
+        for (let [key, value] of prodObj) {
             if (result[prod][key] && Number.isFinite(value)) {
                 result[prod][key] += value;
             } else {
@@ -25,9 +29,44 @@ const combine = (valObj) => { // combines group and sums values
             }
         }
     });
+    console.log({ result: result })
     return result;
 };
 
-export const groupAndCount = (data, groupBy) => combine(Object.values(groupedBy(data, groupBy))[0])
+const splitGroups = (data, groupBy) => {
+    let res = {}
+    Object.values(groupedBy(data, groupBy)).forEach((result, i) => {
+        Object.assign(res, combine(result, i))
+    })
+    return res
+}
 
-console.log(groupAndCount(testData, "name"))
+console.log({ splitGroups: splitGroups(testData, "name") })
+
+const combine2 = (valObj) => { // combines group and sums values
+    let result = {};
+    valObj.map((prods, i) => {
+        const resultObj = result;
+        console.log("Yo2")
+        prods.forEach((products, j) => {
+
+            const prodObj = Object.entries(products)
+
+            for (let [key, value] of prodObj) {
+                console.log({ products2: products.productId })
+            }
+        });
+
+    })
+    return result;
+};
+
+// const groupAndCount = (data, groupBy) => combine(Object.values(groupedBy(data, groupBy))[0])  // only handles first index (one group)
+const groupAndCount2 = (data, groupBy) => combine2(Object.values(groupedBy(data, groupBy)))  // should handle all groups
+
+const runGroup2 = groupAndCount2(testData, "name")
+
+
+console.log({ runGroup2: runGroup2 }) // RUN THIS TO EXECUTE ALL CALLBACKS
+
+
