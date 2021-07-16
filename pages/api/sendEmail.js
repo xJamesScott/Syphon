839b55aec6be384b50176d9764a6d039c24e67d8
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-07-16 10:03:30 -0400

    email api and dynamic data logic

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -3,6 +3,8 @@ import { email as emailBody } from '/public/email/orderConfirmation';
 
 export default async function sendEmail(req, res) {
 
+    const { query: { } } = req
+
     // console.log({"yoooooo!": emailBody})
 
     // return res.json(emailBody);
@@ -25,39 +27,44 @@ export default async function sendEmail(req, res) {
     async function postData(url = '', data = {}) {
         // Default options are marked with *
         const response = await fetch(url, {
-            method: 'GET', 
-            mode: 'cors', 
-            cache: 'no-cache', 
-            credentials: 'same-origin', 
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json'
             }
         });
 
-        const data = response.text();
+        return response.text();
 
-        const emailTagMap = {
-            "{{numItems}}": numItems,
-            "{{address}}": address,
-            "{{total}}": total
-        };
-
-        const mapKeysToString = Object.keys(emailTagMap).join("|");
-
-        const mapRegEx = new RegExp(mapKeysToString, "gi"); // creates regex of keys to be replaced
-
-        const emailWithData = data.replace(mapRegEx, (match) => { // replaces tags in email with customer's data
-            return emailTagMap[match];
-        });
-
-        return emailWithData.text(); // TODO: pass email with data to "value" in "personalizations" array
+        // return emailWithData; // TODO: pass email with data to "value" in "personalizations" array
     }
 
-    postData(endpoint)
+    // postData(endpoint)
+    const emailBody = postData(endpoint)
         .then(data => {
-            res.json(data);
+            const numItems = "888888"
+            const address = "777777777777"
+            const total = "1111111111111"
+            const emailTagMap = {
+                "{{numItems}}": numItems,
+                "{{address}}": address,
+                "{{total}}": total
+            };
+            const mapKeysToString = Object.keys(emailTagMap).join("|");
+            const mapRegEx = new RegExp(mapKeysToString, "gi"); // creates regex of keys to be replaced
+            const emailWithData = data.replace(mapRegEx, (match) => { // replaces tags in email with customer's data 
+                return emailTagMap[match];
+            });
+            
+            return res.json(emailWithData);  // TODO: pass email with data to "value" in "personalizations" array
+            // TODO: return only emailData. use res.status(200) in sendGrid API request.
+        })
 
-        });
+        .catch(err => console.log("Email Error!: " + err));
+
+    return
 
 
     const email = {
@@ -79,21 +86,22 @@ export default async function sendEmail(req, res) {
     // console.log({"ressss!!!!!": res.json(emailBody)})
     // res.json(emailBody);
 
-    //     try {
-    //         const sendGridRequest = await axios({
-    //             method: 'post',
-    //             url: 'https://api.sendgrid.com/v3/mail/send',
-    //             data: email,
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`
-    //             },
-    //         });
-    //         // return res.json(sendGridRequest);
-    //         return res.json("Confirmation Email Sent.");
-    //     } catch (error) {
-    //         // console.log("Send Grid Error! " + error);
-    //         console.log(error);
-    //         return res.status(500);
-    //     }
+        try {
+            const sendGridRequest = await axios({
+                method: 'post',
+                url: 'https://api.sendgrid.com/v3/mail/send',
+                data: email,
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`
+                },
+            });
+            // return res.json(sendGridRequest);
+            return res.json("Confirmation Email Sent.");
+        } catch (error) {
+            // console.log("Send Grid Error! " + error);
+            console.log(error);
+            return res.status(500);
+        }
+
 };
