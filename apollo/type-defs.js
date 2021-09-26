04344a0a2b77788f3b929eecc78b31ebdee39441
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-09-25 20:25:18 -0400

    added cart and item types
    Signed-off-by: xJamesScott <50638563+xJamesScott@users.noreply.github.com>

--- apollo/type-defs.js
+++ apollo/type-defs.js
@@ -1,10 +1,45 @@
 import { gql } from '@apollo/client'
 
+
+
+
 export const typeDefs = gql`
   type User {
     id: ID!
     name: String!
     status: String!
+  }
+
+  enum ProductType {
+    earphones
+    headphones
+    speakers
+  }
+
+  type Cart {
+    productId: String
+    name: String
+    price: Number
+    productType: ProductType
+    quantity: Number
+  }
+
+  type Item {
+    name: String
+    productId: String
+    description: String
+    price: Number
+    quantity: Number
+    productType: ProductType
+    newProduct: Boolean
+    features: String
+    inTheBox: Array
+    featureIMG: Object
+    supportIMG1: Object
+    supportIMG2: Object
+    thumbnailIMG: Object
+    alsoIMG: Object
+    type: Object
   }
 
   type Query {
